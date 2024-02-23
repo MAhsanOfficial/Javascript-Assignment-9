acb0891cb8612dab955f16b9f957fbb2d16cc516
MAhsanOfficial <155831296+MAhsanOfficial@users.noreply.github.com>
2024-02-23 09:59:43 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+ 
+const softwareCompany = {
+    name: "Technical Limited",
+    founder: "M Ahsan",
+    yearFounded: 2000,
+    products: ["Cloud Native", "IT Services", "Backend Development Tools"]
+  };
+  
+   
+ 
+   console.log("Name of the Company is:", softwareCompany.name);
+  console.log("This Name of the Founder is:", softwareCompany.founder);
+  console.log("Company was founded in :", softwareCompany.yearFounded);
+  console.log("This Product company Are:", softwareCompany.products.join(", "));
+  
+   
+  const car = {
+    brand: "Honda",
+    model: "Civik",
+    year: 2024,
+        drive: function() {
+      console.log("The", this.brand, this.model, "is being driven.");
+    }
+  };
+  
+  
+  car.drive();
+
+ 
+ 
+  
+
+
+  
+  
